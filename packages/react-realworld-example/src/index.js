5878e3f9f352689c475f88528d7286d1d1ae2460
Dung Huynh <dunghd.it@gmail.com>
2018-06-11 20:49:03 +0700

    [Enh] Add server rendering with react-snap

--- packages/react-realworld-example/src/index.js
+++ packages/react-realworld-example/src/index.js
@@ -1,8 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './styles/index.css'
-import App from './components/App'
-import registerServiceWorker from './registerServiceWorker'
-
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+import React from "react";
+import { hydrate, render } from "react-dom";
+import "./styles/index.css";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
+/* global document */
+/* eslint-disable react/jsx-filename-extension */
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
+registerServiceWorker();
--- packages/react-realworld-example/src/utils/date.js
+++ packages/react-realworld-example/src/utils/date.js
@@ -1,7 +1,5 @@
-export const today = () => new Date()
+export const today = () => new Date();
 
-export const tomorrow = () =>
-  new Date(today().getTime() + 1 * 24 * 60 * 60 * 1000)
+export const tomorrow = () => new Date(today().getTime() + 1 * 24 * 60 * 60 * 1000);
 
-export const nextWeek = () =>
-  new Date(today().getTime() + 7 * 24 * 60 * 60 * 1000)
+export const nextWeek = () => new Date(today().getTime() + 7 * 24 * 60 * 60 * 1000);

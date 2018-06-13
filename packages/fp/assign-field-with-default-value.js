fdc961efb8ea42ae70bfa004e2a0d5c42ea85007
Dung Huynh <dunghd.it@gmail.com>
2018-06-13 10:27:44 +0700

    [New] Add first solution without using ramda

--- packages/fp/assign-field-with-default-value.js
+++ packages/fp/assign-field-with-default-value.js
@@ -1,3 +1,6 @@
-export default function assignFieldWithDefaultValue(obj) {
+const isFieldExist = (obj, field) => !!obj[field];
+
+export default function assignFieldWithDefaultValue(obj, field, defaultValue) {
+  if (!isFieldExist(obj, field)) return { ...obj, [field]: defaultValue };
   return obj;
 }

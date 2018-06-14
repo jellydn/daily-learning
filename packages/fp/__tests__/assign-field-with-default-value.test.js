c39a6409e94b95078efa5cccc7b9fd2f6e7c6a8a
Dung Huynh <dunghd.it@gmail.com>
2018-06-14 11:00:09 +0700

    Fix xo linter and example with ramda

--- packages/fp/__tests__/assign-field-with-default-value.test.js
+++ packages/fp/__tests__/assign-field-with-default-value.test.js
@@ -1,4 +1,6 @@
-import assignField from '../assign-field-with-default-value';
+import assignField, {
+  assignFieldWithDefault
+} from '../assign-field-with-default-value';
 
 describe('assignField', () => {
   it('should assign property without mutate value', () => {
@@ -7,8 +9,18 @@ describe('assignField', () => {
     const emptyUser = {};
     const newUser = assignField(emptyUser, 'firstName', firstName);
     const newUser2 = assignField(emptyUser, 'lastName', lastName);
-    expect(newUser).toEqual({ firstName });
-    expect(newUser2).toEqual({ lastName });
+    expect(newUser).toEqual({firstName});
+    expect(newUser2).toEqual({lastName});
+    expect(emptyUser).toEqual({});
+  });
+  it('should assign property without mutate value by ramda', () => {
+    const firstName = 'Kaka';
+    const lastName = 'Huynh';
+    const emptyUser = {};
+    const newUser = assignFieldWithDefault(emptyUser, 'firstName', firstName);
+    const newUser2 = assignFieldWithDefault(emptyUser, 'lastName', lastName);
+    expect(newUser).toEqual({firstName});
+    expect(newUser2).toEqual({lastName});
     expect(emptyUser).toEqual({});
   });
 });
--- packages/fp/assign-field-with-default-value.js
+++ packages/fp/assign-field-with-default-value.js
@@ -1,4 +1,14 @@
-const isFieldExist = (obj, field) => !!obj[field];
+const R = require('ramda');
+
+const isFieldExist = (obj, field) => Boolean(obj[field]);
+
+export function assignFieldWithDefault(obj, field, defaultValue) {
+  return R.when(
+    R.has(isFieldExist),
+    result => result,
+    result => ({ ...result, [field]: defaultValue })
+  )(obj);
+}
 
 export default function assignFieldWithDefaultValue(obj, field, defaultValue) {
   if (!isFieldExist(obj, field)) return { ...obj, [field]: defaultValue };
